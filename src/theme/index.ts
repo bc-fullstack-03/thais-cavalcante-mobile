1b665bf23422b6a20f40ef64b0b8c2897c4b2141
Thais Cavalcante <thaispcavalcante@gmail.com>
2023-05-29 21:38:15 -0300

    Creating Theme

---
+++ src/theme/index.ts
@@ -0,0 +1,22 @@
+export const THEME = {
+  COLORS: {
+    BACKGROUND_900: "#121214",
+    BACKGROUND_600: "#202024",
+    TEXT: "#EEEEEE",
+    INPUT: "#7C7C8A",
+    CAPTION_400: "#7C7C8A",
+    BUTTON: "#81D8F7",
+  },
+
+  FONT_FAMILY: {
+    REGULAR: "Inter_400Regular",
+    SEMI_BOLD: "Inter_600SemiBold",
+    BOLD: "Inter_700Bold",
+  },
+
+  FONT_SIZE: {
+    SM: 14,
+    MD: 16,
+    LG: 24,
+  },
+};
