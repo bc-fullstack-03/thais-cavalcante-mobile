6bb2657c1d2df999a2c0c4cd0f4d8191979212c6
Thais Cavalcante <thaispcavalcante@gmail.com>
2023-05-31 20:04:25 -0300

    Adding Stack Navigator to Home

--- src/Screens/Home/index.tsx
+++ src/Screens/Home/index.tsx
@@ -1,10 +1,19 @@
-import { View, Text } from "react-native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Feed from "../Feed";
+import CreatePost from "../CreatePost";
+
+const Stack = createNativeStackNavigator();
 
 function Home() {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Home</Text>
-    </View>
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <Stack.Screen name="Feed" component={Feed} />
+      <Stack.Screen name="CreatePost" component={CreatePost} />
+    </Stack.Navigator>
   );
 }
 
