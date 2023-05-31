bf3bfb7b8ce04a5b030757cfdb5686d0c0a4a159
Thais Cavalcante <thaispcavalcante@gmail.com>
2023-05-31 15:23:42 -0300

    Creating getProfiles function

--- src/services/profile.ts
+++ src/services/profile.ts
@@ -8,3 +8,12 @@ export async function getProfile(profileId: string, authHeader: AuthHeader) {
     throw err;
   }
 }
+
+export async function getProfiles(authHeader: AuthHeader) {
+  try {
+    const { data } = await api.get("/profiles", authHeader);
+    return data;
+  } catch (err) {
+    throw err;
+  }
+}
