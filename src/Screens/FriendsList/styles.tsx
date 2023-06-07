0e753ec87cfb8ed50330a49757ac82aed98792b8
Thais Cavalcante <thaispcavalcante@gmail.com>
2023-06-06 21:30:46 -0300

    Removing unused style properties

--- src/Screens/FriendsList/styles.tsx
+++ src/Screens/FriendsList/styles.tsx
@@ -11,31 +11,4 @@ export const styles = StyleSheet.create({
     fontFamily: THEME.FONT_FAMILY.BOLD,
     fontSize: THEME.FONT_SIZE.LG,
   },
-  profileCard: {
-    margin: 12,
-    gap: 4,
-  },
-  profileIdentification: {
-    flexDirection: "row",
-    alignItems: "center",
-    marginStart: 12,
-  },
-  profileNameText: {
-    color: THEME.COLORS.WHITE,
-    fontFamily: THEME.FONT_FAMILY.BOLD,
-    fontSize: THEME.FONT_SIZE.MD,
-    marginStart: 8,
-  },
-  followers: {
-    color: THEME.COLORS.WHITE,
-    fontFamily: THEME.FONT_FAMILY.REGULAR,
-    fontSize: THEME.FONT_SIZE.SM,
-    marginStart: 8,
-  },
-  following: {
-    color: THEME.COLORS.WHITE,
-    fontFamily: THEME.FONT_FAMILY.REGULAR,
-    fontSize: THEME.FONT_SIZE.SM,
-    marginStart: 8,
-  },
 });
